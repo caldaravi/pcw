1c37d8a0dd33fba74b7582037202bd938f0f4f9b
Marcosaurios <marcosuriosgomez@gmail.com>
2018-04-11 01:50:13 +0200

    Empezadas index y receta con JS

---
+++ practica02/javascript/receta.js
@@ -0,0 +1,159 @@
+function compruebaid(){
+  var url_string = window.location.href;
+  var url = new URL(url_string);
+  var id = url.searchParams.get("id");
+  if(id!=undefined)
+  {
+    // Receta.html tiene un id pasado por GET -> se rellenan los datos
+    // GET del API rest
+    var url_inf = 'rest/receta/'+id;
+    var inf_data = null;
+    fetch(url_inf)
+      .then(
+        function(response){
+          if(response.status !== 200){
+            console.log("No se ha podido realizar la peticion GET correctamente.");
+          }
+          response.json().then(function(data) {
+
+            inf_data = data.FILAS[0];
+            console.log(inf_data);
+
+            // Si ha ido OK -> GET de fotos de esa receta
+            var url_fotos = 'rest/receta/'+id+'/fotos';
+            var fotos_data = null;
+            fetch(url_fotos)
+              .then(
+                function( fotos_resp ){
+                  if( fotos_resp.status !== 200){
+                    console.log("Error STATUS GET fotos con id.");
+                  }
+                  fotos_resp.json().then( function(fotos) {
+
+                    fotos_data = fotos.FILAS;
+                    console.log(fotos_data);
+
+                    // Si ha ido OK -> GET de comentarios
+                    var url_coments = 'rest/receta/'+id+'/comentarios';
+                    var coments_data = null;
+                    fetch(url_coments)
+                      .then(
+                        function( coments_resp ){
+                          if(coments_resp.status !== 200){
+                            console.log("Error STATUS GET comentarios.");
+                          }
+                          coments_resp.json().then( function( comentarios ){
+                              //console.log(comentarios);
+
+                              coments_data = comentarios.FILAS;
+                              console.log(coments_data);
+
+
+                              // =====================================
+                              // SI TODOS LOS GETS DE ANTES HAN IDO BIEN
+                              // =====================================
+
+
+                                          // Obtenemos elementos del HTML
+                                          // Calculamos string comentarios:
+                                          var txt = " ";
+                                          for( var i = 0; i < coments_data.length; i++ ){
+                                            txt +=
+                                            `
+                                              <div>
+                                                <p><span class="icon-user"></span> <a href="buscar.html?a=` + coments_data[i].autor + `"><strong>` + coments_data[i].autor + `</strong></a> dice:</p>
+                                                <p><strong>` + coments_data[i].titulo + `</strong></p>
+                                                <p>` + coments_data[i].texto + `</p>
+                                                <time datetime="` + coments_data[i].fecha + `">` + coments_data[i].fecha + `</time>
+
+                                              </div>
+                                              <hr>
+                                            `;
+                                          }
+                                          let docu = document.getElementById("receta");
+                                          docu.innerHTML +=
+                                          `
+                                          <article class="card">
+                                          <div>
+
+                                            <div>
+                                              <h3>` + inf_data.nombre + `</h3>
+                                                <p>` + inf_data.comensales + `s</p>
+                                                <img src="fotos/` + inf_data.fichero + `" alt="` + inf_data.descripcion_foto + `">
+                                                <a href="">&lt;</a>
+                                                <a href="">&gt;</a>
+                                            </div>
+
+                                            <div>
+                                              <div>
+                                                <h4><span class="icon-user" aria-hidden="true"></span><a href="buscar.html?a=` + inf_data.autor + `">` + inf_data.autor + `</a></h4>
+                                                <p><span class="icon-clock"></span> ` + inf_data.tiempo + ` </p>
+                                                <p><span class="icon-star-empty"></span>Nivel: ` + inf_data.dificultad + `</p>
+                                              </div>
+                                              <div>
+                                                <ul>
+                                                  <li>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nisi, exercitationem.</li>
+                                                  <li>Architecto cum ab voluptatem maxime, numquam saepe mollitia maiores tempora?</li>
+                                                  <li>Sit sed voluptate odio, culpa libero, voluptas optio unde dolores.</li>
+                                                  <li>Veniam blanditiis est tenetur enim quisquam doloremque fugiat maxime cupiditate.</li>
+                                                  <li>Modi provident odit nisi labore obcaecati libero quos numquam similique.</li>
+                                                  <li>Inventore architecto obcaecati suscipit placeat alias atque, laborum odit explicabo.</li>
+                                                </ul>
+                                              </div>
+                                      				<p>
+                                              <a href="">
+                                              <span class="icon-thumbs-up"></span>
+                                              </a> ` + inf_data.positivos + ` likes<a href="">
+                                              <span class="icon-thumbs-down"></span>
+                                              </a> ` + inf_data.negativos + ` dislikes
+                                              </p>
+                                            </div>
+
+                                          </div>
+                                          <div>
+
+                                            <p>` + inf_data.elaboracion + `</p>
+                                            <p><time datetime="` + inf_data.fecha + `">` + inf_data.fecha + `</time></p>
+
+                                          </div>
+
+                                          </article>
+                                          <div id="comentarios" class="card">
+                                            <h4>Comentarios</h4>
+                                          ` + txt + `</div>
+
+                                          <div id="comenta" class="card">
+                                      			<h4>Añade tu comentario</h4>
+                                      			<form action="anadecomentario.php">
+                                      				<input type="text" placeholder="Título..." required>
+                                      				<textarea name="comentario" id="comentario" cols="30" rows="10" placeholder="Comentario..." required></textarea>
+                                      				<input type="submit">
+                                      			</form>
+                                      		</div>
+
+                                          `;
+
+
+
+
+                          });
+                        }
+                      )
+                      .catch( function(err){
+                        console.log("Error GET Comentarios.", err);
+                      })
+                  });
+                }
+              )
+              .catch( function(err){
+                console.log("Error en GET de fotos", err);
+              })
+          });
+        }
+      )
+      .catch(function(err){
+        console.log("Error en GET id receta", err);
+      });
+  }
+
+}
--- practica02/javascript/recetas.js
+++ practica02/javascript/recetas.js
@@ -1,18 +1,74 @@
 function getRecetas()
 {
-  console.log("hola");
-  xhr = new XMLHttpRequest(),
   url = 'rest/receta/?u=6';
 
-  xhr.open('GET', url, true);
+  fetch(url)
+  .then(
+    function( response ){
+      if(response.status !== 200){
 
-  xhr.onload = function(){
-    console.log(xhr.responseText);
-    let r = JSON.parse(xhr.responseText);
+      }
+      response.json().then(function(data){
+        //console.log(data);
 
-    //if(r.RESULTADO == 'OK'){
-      console.log(r);
+    var cont = document.querySelectorAll("article");
 
-    //}
-  }
+    var i;
+    for(i=0;i<data.FILAS.length;i++)
+    {
+      cont[i].innerHTML +=
+      `
+      <a href="receta.html?id=` + data.FILAS[i].id + `"><h3>` + data.FILAS[i].nombre + `</h3></a>
+      <img src="fotos/` + data.FILAS[i].fichero + `" alt="` + data.FILAS[i].nombre + `">
+      <p>`+ data.FILAS[i].descripcion_foto + `</p>
+      <div>
+        <span class="icon-user" aria-hidden="true"></span><a href=buscar.html?="` + data.FILAS[i].autor + `">` + data.FILAS[i].autor + `</a></h4>
+      </div>
+      <footer>
+        <div>
+          <div>
+            <p><a href=""><span class="icon-thumbs-up"></span></a> ` + data.FILAS[i].positivos + ` likes</p>
+            <p><span class="icon-comment"></span> ` + data.FILAS[i].comentarios + ` comentarios</p>
+          </div>
+          <div>
+            <p><a href=""><span class="icon-thumbs-down"></span></a>` + data.FILAS[i].negativos + ` dislikes</p>
+        <p><time datetime="` + data.FILAS[i].fecha + `">` + data.FILAS[i].fecha + `</time></p>
+          </div>
+        </div>
+      </footer>
+      `;
+      console.log(cont[i]);
+    }
+
+    // Paginacion
+    var total = cont.length;
+    let paginas = total / 6;
+      // Ceil redondea al entero mayor (si hay 13 resultados, 13/6 = 2,... habrán 3 páginas)
+    paginas = Math.ceil(paginas);
+    var docu = document.getElementsByClassName("pagination");
+    if(paginas == 1 )
+    {
+        docu[0].innerHTML +=
+        `
+        <a href="#">1</a>
+        `;
+    }
+    else {
+      docu[0].innerHTML += '<a href="#">&laquo;</a>';
+      for(var i = 1; i<=paginas; i++)
+      {
+        docu[0].innerHTML += '<a href="#">' + i + '</a>';
+      }
+      docu[0].innerHTML += '<a href="#">&raquo;</a>';
+    }
+    console.log(paginas);
+
+
+    });
+})
+.catch(function() {
+    // This is where you run code if the server returns any errors
+
+    console.log('ERROR');
+});
 }
